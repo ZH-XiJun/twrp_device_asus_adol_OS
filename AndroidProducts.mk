d2caa64b04ecbaf5a4ff735d880495abd58e948b
wt_bruhhhh <59949135+ZH-XiJun@users.noreply.github.com>
2024-12-14 19:12:45 +0800

    Initial commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_adol_OS.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_adol_OS-user \
+    omni_adol_OS-userdebug \
+    omni_adol_OS-eng
---
+++ vendorsetup.sh
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2024 The Android Open Source Project
+# Copyright (C) 2024 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+add_lunch_combo omni_adol_OS-user
+add_lunch_combo omni_adol_OS-userdebug
+add_lunch_combo omni_adol_OS-eng
